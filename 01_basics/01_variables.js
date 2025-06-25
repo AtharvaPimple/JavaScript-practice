82ef0e4ce80b5d7dd26602ba7b7f7cb29d51856c
= <=>
2025-06-25 22:40:05 +0530

    Update 01_basics: Add variable declarations and test cases

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,11 @@
 const accountId = 13132;
+let accountEmail = "atharva.pimple@gmail.com";
+var accountPassword = "1234abcd";
+accountCity = "Pune";
+
+// accountId = 12345; // Reassigning a variable declared with const will throw an error
+accountEmail = "ap@ap.com";
+accountPassword = "newpassword"; // Reassigning a variable declared with var is allowed
+accountCity = "Mumbai"; // Reassigning a variable declared with let is allowed
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,6 @@
 console.log("Hello, World!");
 console.log("This is a test file.");
+console.log("2" + 2);
+console.log("2" - 2);
+console.log("200.08776" * 2);
+console.log("2000" / 2);
